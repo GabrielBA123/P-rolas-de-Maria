042fda011b490c889e1c754db1e5d9fa0436819f
GabrielBA123 <gb958643@gmail.com>
2026-07-22 21:13:19 -0300

    atualização

--- admin/js/supabase-client.js
+++
@@ -1,8 +0,0 @@
-/* ==========================================================================
-   supabase-client.js
-   ========================================================================== */
-
-const SUPABASE_URL = 'https://ldjubhvkkrxbpgrcdnid.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_jzaLV-9q_ANf4bftlPOMtg_471J_g9g';
-
-const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
